44e8e7ba4afd6b2241634b986a18b8afca3d4113
Kamil Wenta <kamil.wenta222@gmail.com>
2021-11-02 16:50:58 +0100

    Make board more responsive
    Add abuttons for available moves
     Change look of fields which are not for sale

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,19 +7,56 @@ interface Props extends BoardProps {
   G: GameState;
 }
 
-export const Game = ({ G, ctx }: Props): ReactElement => {
+type Stages =
+  | "rollDice"
+  | "noAction"
+  | "isOwner"
+  | "hasOwner"
+  | "noOwner"
+  | "cardField"
+  | "auction";
+
+export const Game = ({ G, ctx, moves }: Props): ReactElement => {
   const currentPlayer = G.players[parseInt(ctx.currentPlayer)];
-  const currentStage =
-    ctx.activePlayers?.[ctx.currentPlayer] || ctx.phase || "rollDice";
+  const currentStage = (ctx.activePlayers?.[ctx.currentPlayer] ||
+    ctx.phase ||
+    "rollDice") as Stages;
+  const stageMoves = {
+    rollDice: ["rollDice"],
+    noAction: ["endTurn", "bankrupt"],
+    isOwner: ["endTurn", "bankrupt", "buyHouse", "sellHouse"],
+    hasOwner: ["pay"],
+    noOwner: ["buyProperty", "auction"],
+    cardField: ["drawCard", "acceptCard"],
+    auction: ["bid", "pass"],
+  };
   return (
-    <main className="flex justify-center items-center p-4">
-      <div className="grid board-shape gap-1">
+    <main className="flex justify-center items-center h-screen">
+      <div className="grid board-shape gap-1 max-h-screen h-[90%]">
         {G.fields.map((field, i) => (
-          <Tile key={i} field={field} />
+          <Tile key={i} field={field} index={i} />
         ))}
-        <div className="board-center text-center">
-          <h1 className="text-4xl">{currentPlayer.name}</h1>
-          <p>{currentStage}</p>
+        <div className="board-center text-center flex">
+          <section className="flex-1">
+            <h1 className="text-4xl">{currentPlayer.name}</h1>
+            <p>{currentStage}</p>
+            <div className="flex flex-col gap-2">
+              {stageMoves[currentStage].map(v => (
+                <button
+                  key={v}
+                  className="bg-gradient-to-br from-red-600 via-red-400 to-red-700 px-3 py-2 text-lg rounded-lg shadow-md"
+                  onClick={moves[v]}
+                >
+                  {v}
+                </button>
+              ))}
+            </div>
+          </section>
+          <section className="flex-1">
+            <h2 className="text-3xl">
+              {G.fields[currentPlayer.position].name}
+            </h2>
+          </section>
         </div>
       </div>
     </main>
@@ -28,10 +65,31 @@ export const Game = ({ G, ctx }: Props): ReactElement => {
 
 interface TileProps {
   field: Field;
+  index: number;
 }
 
-const Tile = ({ field }: TileProps) => (
-  <div className="text-black bg-white rounded-lg flex flex-row">
-    <div>$ {field.price}</div>
-  </div>
-);
+const Tile = ({ field, index }: TileProps) => {
+  const isBuyable = !!field.price;
+  return (
+    <div
+      className={`square h-full text-black bg-white rounded-2xl flex flex-col justify-between ${
+        isBuyable
+          ? "after:content-end after:bg-black after:w-full after:h-4 after:rounded-b-2xl relative before:absolute before:bg-white before:w-full before:h-2 before:rounded-b-xl before:bottom-[9px]"
+          : "pb-[21px]"
+      }`}
+    >
+      <div
+        className={`flex justify-between ${
+          isBuyable
+            ? "after:content-end after:bg-gray-400 after:w-5 after:h-5 after:rounded-tr-2xl"
+            : "mt-5"
+        }`}
+      >
+        <span className="text-center text-sm flex-1">
+          {isBuyable && `$ ${field.price}`}
+        </span>
+      </div>
+      <span className="text-center text-3xl text-black/40">{index}</span>
+    </div>
+  );
+};
