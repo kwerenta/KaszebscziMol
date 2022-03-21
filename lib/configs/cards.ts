f9fc49b531d87493dc7400db6f0d865c194c1afd
Kamil Wenta <kamil.wenta222@gmail.com>
2022-03-21 13:46:07 +0100

    Tweaks to card system
    Rename some card actions
    Add advanceToNearest card action
    Fix some typos in card texts
    Fix some issues with existing card actions
    Add some new cards

--- lib/configs/cards.ts
+++ lib/configs/cards.ts
@@ -1,81 +1,101 @@
-export interface Card {
-  text: string;
-  action: "substract" | "add" | "payAll" | "getAll" | "goToJail";
-  amount: number;
-}
+import { Groups, PropertyGroups } from "./spaces";
+
+type actionsWithNumber =
+  | "collect"
+  | "collectFromEveryPlayer"
+  | "pay"
+  | "payEachPlayer";
+
+type Base<T> = { text: string } & T;
+
+export type Card = Base<
+  | {
+      action: actionsWithNumber;
+      payload: number;
+    }
+  | { action: "advanceToNearest"; payload: Groups }
+  | { action: "goToJail"; payload: undefined }
+>;
 
 export const cards: Card[] = [
-  { text: "Wygrywasz Snuff World Championship! Otrzymujesz 500$", action: "add", amount: 500 },
   {
-    text:
-      "Turysta zapytał się, którędy do stolicy Kaszub. Pokazując trzy kierunki świata zostałeś okradziony. Tracisz 250$",
-    action: "substract",
-    amount: 250,
+    text: "Wygrywasz Snuff World Championship! Otrzymujesz 500$",
+    action: "collect",
+    payload: 500,
   },
   {
-    text:
-      "Zostałeś zaproszony na Browargedon. Zaproszenie zostało natychmiast przyjęte, a tytuł mistrzowski trafia w twoje ręcej. Jako nagrodę dostajesz 300$",
-    action: "add",
-    amount: 300,
+    text: "Turysta zapytał się, którędy do stolicy Kaszub. Pokazując trzy kierunki jednocześnie, wczasowicz okardł Cię. Tracisz 250$",
+    action: "pay",
+    payload: 250,
   },
   {
-    text:
-      "W trakcie Bimbergedonu nie udało Ci się pokonać pierwszej konkurencji, jaką było wejście po schodach. Dla odbudowania wizerunku płacisz każdemu graczowi 50$",
-    action: "payAll",
-    amount: 50,
+    text: "Otrzmujesz wezwanie na Browargedon. Zaproszenie zostało natychmiast przez Ciebie przyjęte, a tytuł mistrzowski trafia w twoje ręcej. Jako nagrodę dostajesz 300$",
+    action: "collect",
+    payload: 300,
   },
   {
-    text:
-      "Deskę sedesowa w toalecie gospodarza imprezy została przez Ciebie uszkodzona. Pokrywasz koszty i tracisz 100$",
-    action: "substract",
-    amount: 100,
+    text: "W trakcie Bimbergedonu nie udało Ci się pokonać pierwszej konkurencji, jaką było wejście po schodach. Dla odbudowania wizerunku płacisz każdemu graczowi 50$",
+    action: "payEachPlayer",
+    payload: 50,
   },
   {
-    text:
-      "Po wyjściu z Biedronki orientujesz się, że zakupiony zapas Be Power został objęty podatkiem cukrowym. Przeznaczno 50$ na rozwój państwa",
-    action: "substract",
-    amount: 50,
+    text: "Deska sedesowa w toalecie gospodarza imprezy została przez Ciebie uszkodzona. Pokrywasz koszty i tracisz 100$",
+    action: "pay",
+    payload: 100,
   },
   {
-    text:
-      "Dzięki zdobyciu wyniku 120% na praktyce zawodowej w ZSŁ, otrzymujesz międzynarodowy certyfikat. Reszta graczy z szacunku i zazdrości przekazuje Ci po 50$",
-    action: "getAll",
-    amount: 50,
+    text: "Po wyjściu z Biedronki orientujesz się, że zakupiony zapas Be Powera został objęty podatkiem cukrowym. Przeznaczasz 50$ na rozwój państwa",
+    action: "pay",
+    payload: 50,
   },
   {
-    text:
-      "Inwestujesz w budowę restauracji McDonalds w Kartuzach. Lokalni rolnicy zaczęli strajkować i grozić widłami przed twoim domem. Mieszkańcy ogłosili, że obecne od lat 70. bary mleczne są zdecydowanie lepsze od jakiegoś fast foodu. Z powodu nieudanej inwestycji tracisz 250$",
-    action: "substract",
-    amount: 350,
+    text: "Dzięki zdobyciu wyniku 120% na praktyce zawodowej w ZSŁ, otrzymujesz międzynarodowy certyfikat. Reszta graczy z szacunku i zazdrości przekazuje Ci po 50$",
+    action: "collectFromEveryPlayer",
+    payload: 50,
   },
   {
-    text:
-      "Udało Ci się zdobyć lody Ekipy. Po ich spożyciu, wystawiasz na serwis aukcyjny lekko zgnieciony papierek. Zdesperowana matka kupuje go swojemu dziecku na urodziny za 100$",
-    action: "add",
-    amount: 100,
+    text: "Inwestujesz w budowę restauracji McDonalds w Kartuzach. Lokalni rolnicy zaczęli strajkować i grozić widłami przed twoim domem. Mieszkańcy ogłosili, że obecne od lat 70. bary mleczne są zdecydowanie lepsze od jakiegoś fast foodu. Z powodu nieudanej inwestycji tracisz 250$",
+    action: "pay",
+    payload: 350,
   },
   {
-    text:
-      "Mieszkając w lesie masz dobre kontakty z Rumunami. Otrzymujesz dostęp do luksusowych towarów takich jak Gerrikoll czy Gavin Kaiqin. Stajesz się resellerem i każdy kupuje od Ciebie roczny zapas za 25$",
-    action: "getAll",
-    amount: 25,
+    text: "Udało Ci się zdobyć lody Ekipy. Po ich spożyciu, wystawiasz na serwis aukcyjny lekko zgnieciony papierek. Zdesperowana matka kupuje go od Ciebie dla swojego dziecka za 100$",
+    action: "collect",
+    payload: 100,
   },
   {
-    text:
-      "Po przejechaniu 20 kilometrów i napotkaniu drogi z górki twoja bestia Opel Astra zaczyna się rozpędzać. Niestety dogania Cię policja i wręcza Ci mandat w wysokości 150$ za utrudnianie ruchu",
-    action: "substract",
-    amount: 150,
+    text: "Mieszkając w lesie masz dobre kontakty z Rumunami. Otrzymujesz dostęp do luksusowych towarów takich jak Gerrikoll czy Gavin Kaiqin. Stajesz się resellerem i każdy kupuje od Ciebie roczny zapas za 25$",
+    action: "collectFromEveryPlayer",
+    payload: 25,
   },
   {
-    text:
-      "Jako mieszkaniec Pomorskiej 13 czujesz, że Twoim obowiązkiem obywatelskim jest sprzedawanie dobrych chłopaków na komendzie. Za swoje zasługi zostajesz pobity, a każdy gracz wziął sobie 50$",
-    action: "payAll",
-    amount: 50,
+    text: "Po przejechaniu 20 kilometrów i napotkaniu drogi z górki twoja bestia Opel Astra zaczyna się rozpędzać. Niestety dogania Cię policja i wręcza Ci mandat w wysokości 150$ za utrudnianie ruchu",
+    action: "pay",
+    payload: 150,
   },
   {
-    text:
-      "Dzień przed Bimbergedonem zostały ogłoszone nowe obostrzenia. Impreza została przez Ciebie odwołana. Mimo to, następnego dnia zjawia się policja, która wręcza Ci mandat za niezorganizowanie fantastycznej imprezy. Płacisz 200$",
-    action: "substract",
-    amount: 200,
+    text: "Jako mieszkaniec Pomorskiej 13 czujesz, że Twoim obowiązkiem obywatelskim jest sprzedawanie dobrych chłopaków na komendzie. Za swoje zasługi zostajesz pobity, a każdy gracz wziął sobie 50$",
+    action: "payEachPlayer",
+    payload: 50,
+  },
+  {
+    text: "Dzień przed Bimbergedonem zostały ogłoszone nowe obostrzenia. Impreza została przez Ciebie odwołana. Mimo to, następnego dnia zjawia się policja, która wręcza Ci mandat za niezorganizowanie fantastycznej imprezy. Płacisz 200$",
+    action: "pay",
+    payload: 200,
+  },
+  {
+    text: "Trafiasz na najbliszą stację kolejową. Najnowsze rozwiązania technologiczne wprawiają Ciebie w zachwyt. Jesteś pod tak ogromnym wrażeniem, że postanawiasz zakupić dworzec. Jeśli ma już właściciela to z chęcią zapłacisz czynsz.",
+    action: "advanceToNearest",
+    payload: PropertyGroups.Train,
+  },
+  {
+    text: "Dojeżdżasz na stację z jedynie 147 minutowym opóźnieniem. Nadprogramowy czas podróży uznajesz za dodatkową przyjemność z jazdy. Uznajesz, że warto kupić dworzec, aby zerowym kosztem doświadczać niezapomniane przeżycia. Jeżeli ktoś jest już jego właścicielem, to płacisz czynsz w ramach podziękowania za jakościowe usługi.",
+    action: "advanceToNearest",
+    payload: PropertyGroups.Train,
+  },
+  {
+    text: "Twój znajomy kurier słusznie stwierdza, że picie czystego soku z cytryny powinno być nielegalne. W wyniku popełnienia tego niewybaczalnego czynu, sam postanawiasz udać się do więzienia.",
+    action: "goToJail",
+    payload: undefined,
   },
 ];
--- lib/moves/cardAction.ts
+++ lib/moves/cardAction.ts
@@ -1,5 +1,5 @@
 import { Move } from "boardgame.io";
-import { INVALID_MOVE } from "boardgame.io/core";
+import { INVALID_MOVE, Stage } from "boardgame.io/core";
 import { cards } from "../configs/cards";
 import { GameState } from "../KaszebscziMol";
 import { getPlayer, goToJail } from "./base";
@@ -7,41 +7,73 @@ import { getPlayer, goToJail } from "./base";
 export const acceptCard: Move<GameState> = (G, ctx) => {
   const currentPlayer = getPlayer(G, ctx);
 
-  const currentCard = cards[G.card];
+  const { payload, action } = cards[G.card];
 
-  switch (currentCard.action) {
-    case "add":
-      currentPlayer.money += currentCard.amount;
+  let nextStage = "noAction";
+
+  switch (action) {
+    case "collect":
+      currentPlayer.money += payload;
       break;
 
-    case "substract":
-      if (currentPlayer.money < currentCard.amount) return INVALID_MOVE;
-      currentPlayer.money -= currentCard.amount;
+    case "pay":
+      if (currentPlayer.money < payload) return INVALID_MOVE;
+      currentPlayer.money -= payload;
       break;
 
-    case "payAll":
-      const amount = ctx.playOrder.length * currentCard.amount;
+    case "payEachPlayer":
+      const amount = (ctx.playOrder.length - 1) * payload;
       if (currentPlayer.money < amount) return INVALID_MOVE;
       ctx.playOrder.forEach(id => {
         if (id === ctx.currentPlayer) currentPlayer.money -= amount;
-        else G.players[id].money += currentCard.amount;
+        else G.players[id].money += payload;
       });
       break;
-    case "getAll":
-      ctx.playOrder.forEach(id => {
-        if (id === ctx.currentPlayer) {
-          currentPlayer.money += ctx.playOrder.length * currentCard.amount;
-        } else {
-          const player = G.players[id];
 
-          if (player.money < currentCard.amount) {
-            console.log(`Error! Player ${id} has not enough money`);
-            return INVALID_MOVE;
-          }
-          player.money -= currentCard.amount;
+    case "collectFromEveryPlayer":
+      Object.entries(G.players).forEach(([id, player]) => {
+        if (id === ctx.currentPlayer) {
+          currentPlayer.money += (ctx.playOrder.length - 1) * payload;
+          return;
         }
+
+        if (player.money < payload) return INVALID_MOVE;
+
+        player.money -= payload;
       });
       break;
+
+    case "advanceToNearest":
+      const numOfSpaces = G.spaces.length;
+      const distanceToNearestSpace = G.spaces.reduce(
+        (shortestDistance, space, index) => {
+          if (space.group !== payload) return shortestDistance;
+          return Math.min(
+            (index + numOfSpaces - currentPlayer.position) % numOfSpaces,
+            shortestDistance
+          );
+        },
+        numOfSpaces
+      );
+      let newPosition = currentPlayer.position + distanceToNearestSpace;
+
+      if (newPosition > 39) {
+        currentPlayer.money += 200;
+        newPosition -= 40;
+      }
+
+      currentPlayer.position = newPosition;
+
+      const currentSpace = G.spaces[currentPlayer.position];
+      nextStage = !currentSpace.price
+        ? "noAction"
+        : currentSpace.owner === ""
+        ? "noOwner"
+        : currentSpace.owner === ctx.currentPlayer
+        ? "isOwner"
+        : "hasOwner";
+      break;
+
     case "goToJail":
       goToJail(currentPlayer, ctx);
       break;
@@ -50,5 +82,5 @@ export const acceptCard: Move<GameState> = (G, ctx) => {
   // set current card to none
   G.card = -1;
 
-  ctx.events.setStage("noAction");
+  ctx.events.setStage(nextStage);
 };
